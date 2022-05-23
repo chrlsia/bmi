f21e909e09fc91ba9c14987698c3700d46568d83
Chris Siannas <xrsia@otenet.gr>
2022-05-23 09:36:25 +0300

    ola ok, next:break getUserMetrics

--- bmi.go
+++ bmi.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/chrlsia/bmi/info"
 )
 
-
 func main() {
-	//Output information
+	
 	info.PrintWelcome()
 
 	weight, height:= getUserMetrics()
-	
 
-	//Calculate the bmi (weight/(height * height))
-	bmi := weight / (height * height)
-	//Output the calculated bmi
-	fmt.Printf("Your BMI : %.2f\n", bmi)
+	bmi := calculateBMI(weight,height)
+
+	printBMI(bmi)
 }
 
+func calculateBMI(weight, height float64) float64{
+	return weight / (height * height)
+}
