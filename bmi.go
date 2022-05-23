094e7baccbcf5440b1f1a1b9e96fa624bb0b7c52
Chris Siannas <xrsia@otenet.gr>
2022-05-23 07:38:54 +0300

    const

--- bmi.go
+++ bmi.go
@@ -10,27 +10,32 @@ import (
 
 var reader = bufio.NewReader(os.Stdin)
 
+const mainTitle = "BMI calculator"
+const separator = "----------------------"
+const weightPrompt = "Ente your weight (kg): "
+const heightPrompt = "Ente your height (m): "
+
 func main() {
 	//Output information
-	fmt.Println("BMI calculator")
-	fmt.Println("----------------------")
+	fmt.Println(mainTitle)
+	fmt.Println(separator)
 
 	//Prompt for user input (weight + height)
-	fmt.Print("Ente your weight (kg): ")
-	weightInput,_:=reader.ReadString('\n')
+	fmt.Print(weightPrompt)
+	weightInput, _ := reader.ReadString('\n')
 
-	fmt.Print("Ente your height (m): ")
-	heightInput,_:=reader.ReadString('\n')
+	fmt.Print(heightPrompt)
+	heightInput, _ := reader.ReadString('\n')
 
 	//Save that user input in variables
-	weightInput = strings.Replace(weightInput,"\n","",-1)
-	heightInput = strings.Replace(heightInput,"\n","",-1)
+	weightInput = strings.Replace(weightInput, "\n", "", -1)
+	heightInput = strings.Replace(heightInput, "\n", "", -1)
 
-	weight,_:= strconv.ParseFloat(weightInput,64)
-	height,_:= strconv.ParseFloat(heightInput,64)
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
 
 	//Calculate the bmi (weight/(height * height))
-	bmi:= weight/(height * height)
-	//Output the calculated bmi 
-	fmt.Printf("Your BMI : %.2f\n",bmi)
+	bmi := weight / (height * height)
+	//Output the calculated bmi
+	fmt.Printf("Your BMI : %.2f\n", bmi)
 }
